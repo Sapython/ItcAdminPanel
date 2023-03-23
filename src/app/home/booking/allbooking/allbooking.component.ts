80cf9fdb603a7c5206d3184121d13a47df6bbf57
Rohan-ulti <rohan@izenbridge.com>
2023-03-23 19:13:53 +0530

    Gautum Dashboard & services Changes

--- src/app/home/booking/allbooking/allbooking.component.ts
+++ src/app/home/booking/allbooking/allbooking.component.ts
@@ -1,6 +1,6 @@
 import { AssignGuideDialogComponent } from './../dialogs/assign-guide-dialog/assign-guide-dialog.component';
 import { AssignDriverDialogComponent } from './../dialogs/assign-driver-dialog/assign-driver-dialog.component';
-import { Component } from '@angular/core';
+import { Component,AfterViewInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -8,9 +8,55 @@ import { MatDialog } from '@angular/material/dialog';
   templateUrl: './allbooking.component.html',
   styleUrls: ['./allbooking.component.scss']
 })
-export class AllbookingComponent {
+export class AllbookingComponent  {
+  data: any =
+    [
+      {
+        "parentName": "Parent One",
+        "childProperties":
+          [
+            { "propertyName": "Property One" },
+            { "propertyName": "Property Two" }
+          ]
+      },
+      {
+        "parentName": "Parent Two",
+        "childProperties":
+          [
+            { "propertyName": "Property Three" },
+            { "propertyName": "Property Four" },
+            { "propertyName": "Property Five" },
+          ]
+      },
+      {
+        "parentName": "Parent Three",
+        "childProperties":
+          [
+            { "propertyName": "Property Six" },
+            { "propertyName": "Property Seven" },
+            { "propertyName": "Property Eight" },
+          ]
+      }
+    ]
+
 
   constructor(private dialog:MatDialog){}
 
+  toggleAccordian(event:any, index:any) {
+    var element = event.target;
+    // element.classList.toggle("active");
+    // if(this.data[index].isActive) {
+    //   this.data[index].isActive = false;
+    // } else {
+    //   this.data[index].isActive = true;
+    // }
 
+    var panel = element.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+
+}
 }
